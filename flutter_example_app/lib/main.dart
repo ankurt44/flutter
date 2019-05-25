cd5dc051eec0492350f3ce225cfb6061edefecef
ank <ankurtiwari044@gmail.com>
2019-05-25 18:27:40 +0200

    Complete google developers example.

--- flutter_example_app/lib/main.dart
+++ flutter_example_app/lib/main.dart
@@ -1,20 +1,124 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyExampleApp());
+void main() => runApp(MyApp());
 
-class MyExampleApp extends StatelessWidget
-{
+// #docregion MyApp
+class MyApp extends StatelessWidget {
+  // #docregion build
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('XXXXXX'),
-        ),
-        body: TextField(),
+      title: 'Startup Name Generator',
+      theme: ThemeData(primaryColor: Colors.white),
+      home: RandomWords(),
+    );
+  }
+  // #enddocregion build
+}
+// #enddocregion MyApp
+
+// #docregion RWS-var
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // or => final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(height: 5.0,); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    final bool isSaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        isSaved?Icons.favorite : Icons.favorite_border, 
+        color : isSaved ? Colors.red[300] : null
+      ),
+      onTap: (){
+        setState(() {
+          if(isSaved)
+            _saved.remove(pair);
+          else
+            _saved.add(pair); 
+        });
+      },
+    );
+  }
+  // #enddocregion _buildRow
+
+  void _pushPressed(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair){
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase, 
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          )
+          .toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Pairs'),
+            ), 
+            body: ListView(
+              children: divided,
+            ),
+          );
+        },
       ),
     );
   }
+
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[IconButton(icon: Icon(Icons.list), onPressed: _pushPressed,)],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  // #enddocregion RWS-build
+  // #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
